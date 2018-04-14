9979614e83b47c238a6ca00e3539b65d7b2c4799
Nikita Monastyrskyy <n.monastyrskyi@gmail.com>
2018-04-14 10:58:25 +0300

    try with 18px

--- legicash/js/common.js
+++ legicash/js/common.js
@@ -2,10 +2,10 @@ $(function() {
 $('.header__menu__item').on('click', 'a[href^="#"]', function (event) {
 	event.preventDefault();
 	let scrollSpeed = 1500;
-	setTimeout(() => {$(this).blur();}, scrollSpeed);
-
+	// setTimeout(() => {$(this).blur();}, scrollSpeed);
+	let scrollTo = $($.attr(this, 'href')).offset().top - $('.header').height()
 	$('html, body').animate({
-		scrollTop: $($.attr(this, 'href')).offset().top
+		scrollTop: scrollTo
 	}, scrollSpeed);
 });
 

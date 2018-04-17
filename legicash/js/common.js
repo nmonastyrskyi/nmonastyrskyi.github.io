e4734c16965dd3c98226071f9ab19020bcd92ef3
Nikita Monastyrskyy <n.monastyrskyi@gmail.com>
2018-04-17 22:55:03 +0300

    finished legicash

--- legicash/js/common.js
+++ legicash/js/common.js
@@ -4,7 +4,9 @@ $(function () {
 	$('.header__menu__item,.mmenu__menu__item').on('click', 'a[href^="#"]', function (event) {
 		event.preventDefault();
 		let scrollSpeed = 1500;
-		setTimeout(() => {$(this).blur();}, scrollSpeed);
+		setTimeout(() => {
+			$(this).blur();
+		}, scrollSpeed);
 		let scrollTo = $($.attr(this, 'href')).offset().top - $('.header').height()
 		$('html, body').animate({
 			scrollTop: scrollTo
@@ -39,34 +41,26 @@ $(function () {
 
 	}
 
-	// /*HIGHLITING NAB ITEMS WHILE SCROLLING*/
-	// $(window).scroll(function () {
-	// 	let $window_top = $(window).scrollTop();
-	// 	let $intro_top = $('#introduction').offset().top
-	// 	let $solution_top = $('#solution').offset().top
-	// 	let $approach_top = $('#approach').offset().top
-	// 	let $team_top = $('#team').offset().top
+	// POPUP HANDLING
+	$('.advisors__photo').on('click', popup)
+	$('.popup').on('click', (e)=>{e.stopPropagation();})
+	$('.popup__container,.popup__close').on('click', closePopup)
 
-	// 	let $solution_offset_top = $solution_top - ($solution_top - $intro_top) * 0.3
-	// 	let $approach_offset_top = $approach_top - ($approach_top - $solution_top) * 0.3
-	// 	let $team_offset_top = $team_top - ($team_top - $approach_top) * 0.3
 
-	// 	if ($window_top >= $intro_top && $window_top < $solution_offset_top) {
-	// 		$('.header__menu__item a').removeClass('active')
-	// 		$('.header__menu__item a:first').addClass('active')
-	// 		// } else if ($window_top >= $galery_offset_top && $window_top < $price_offset_top) {
-	// 		// 	$('.header__nav__item a').removeClass('active')
-	// 		// 	$('.header__nav__item a:eq( 1 )').addClass('active')
-	// 		// } else if ($window_top >= $price_offset_top && $window_top < $reviews_offset_top) {
-	// 		// 	$('.header__nav__item a').removeClass('active')
-	// 		// 	$('.header__nav__item a:eq( 2 )').addClass('active')
-	// 		// } else if ($window_top >= $reviews_offset_top && $window_top < $contacts_ofsset_top) {
-	// 		// 	$('.header__nav__item a').removeClass('active')
-	// 		// 	$('.header__nav__item a:eq( 3 )').addClass('active')
-	// 		// } else if ($window_top >= $contacts_ofsset_top) {
-	// 		// 	$('.header__nav__item a').removeClass('active')
-	// 		// 	$('.header__nav__item a:eq( 4 )').addClass('active')
-	// 	}
-	// 	/*HIGHLIGHT NAV ITEMS WHEN SCROLLING END*/
-	// });
+	function popup() {
+		$('.popup__container').show()
+		$('.popup').animate({
+			opacity: '1'
+		}, 500)
+	}
+
+	function closePopup(e) {
+		e.stopPropagation();
+		$('.popup').animate({
+			opacity: '0'
+		}, 500)
+
+		$('.popup__container').hide(500)
+	}
+	// POPUP HANDLING END
 });
